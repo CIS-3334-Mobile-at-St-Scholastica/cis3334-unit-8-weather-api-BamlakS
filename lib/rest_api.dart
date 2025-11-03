b8ba8d959c09e2bf0c31297ae53608ff57dfad60
BamlakS <bamedie@css.edu>
2025-11-03 03:33:02 -0600

    Completed version of Assignment 8!

---
+++ lib/rest_api.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_weatherapi_f25/weather_model.dart';
+import 'package:http/http.dart' as http;
+
+Future<List<DailyForecast>> fetchWeather() async {
+  final response = await http
+      .get(Uri.parse('https://api.openweathermap.org/data/2.5/forecast?q=duluth&units=imperial&cnt=8&appid=5aa6c40803fbb300fe98c6728bdafce7'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    print('fetchWeather- '+response.body);
+    Weather weather = weatherFromJson(response.body);
+    return weather.daily;
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
